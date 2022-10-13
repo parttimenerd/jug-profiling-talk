8716375ee138368ff10a4d8a11bbece8300e8b4e
Johannes Bechberger <johannes.bechberger@sap.com>
2022-10-13 15:15:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# jug-profiling-talk
-Notes and scripts for my JUG KA talk on Java profiling
+# JUG Karlsruhe Talk
+Notes and scripts for my JUG Karlsruhe talk on Java profiling
+
+## Interesting talks
+I collected a list of interesting talks on profiling in a 
+[YouTube playlist](https://www.youtube.com/playlist?list=PLLLT4NxU7U1QYiqanOw48h0VUjlUvqCCv)
+
+## Interesting blogs
+The following are interesting blogs on Java profiling and performance:
+- Aleksey Shipilev:  https://shipilev.net/
+- Nitsan Wkart:  http://psy-lob-saw.blogspot.com/
+- Richard Startin:  https://richardstartin.github.io/
+- Peter Lawrey:  http://blog.vanillajava.blog/
+- Martin Thompson:  https://mechanical-sympathy.blogspot.com/
+- Jean-Philippe Bempel: https://jpbempel.github.io/
+- Markus Hirt: http://hirt.se/blog/
+
+## Interesting people to follow on Twitter
+Specifically on Twitter
+- [Andrei Pangin](https://twitter.com/AndreiPangin): Creator of async-profiler
+- [JVMPerformance](https://twitter.com/JVMPerformance): JVM performance news (old)
+- [Alexsey Shipilev](https://twitter.com/shipilev)
+- [Jean-Philippe Bempel](https://twitter.com/jpbempel)
+- [Peter Veentjer](https://twitter.com/PeterVeentjer)
+- [Gunnar Morling](https://twitter.com/gunnarmorling)
+- [Chris Newland](https://twitter.com/chriswhocodes)
+  - with his homepage full of helper tools to explore JEPs, VM options, ...: [chriswhocodes.com](https://www.chriswhocodes.com/)
+
+
+## profile.py
+
+Wrapper around JFR and [async-profiler](https://github.com/jvm-profiling-tools/async-profiler) 
+(`$ASYNC_PROFILER` should point to repo with built async-profiler).
+Works only for JDK11+.
+
+Record a simple JFR trace via
+
+```sh
+./profile.py jfr -f file.jfr -- -cp test AllocatingTarget
+```
+
+and a simple async-profiler trace via
+
+```sh
+./profile.py async -s -f file.jfr -- -cp test AllocatingTarget
+```
+
+produce a flamegraph via
+
+```sh
+./profile.py async -f file.html -t flamegraph -- -cp test AllocatingTarget
+```
+
+Requires the click package (install it via `pip3 install click`)
+
+## JFR config files
+
+- `cpu_profiling.jfc`: CPU profiling and nothing more
+- `cpu_and_mem_profiling.jfc`: CPU and memory profiling and nothing more
+
+## License
+Apache 2.0
---
+++ profile.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+import os
+
+import click
+import shlex
+from pathlib import Path
+from typing import List, Optional
+
+"""
+Wrapper around JFR and async-profiler ($ASYNC_PROFILER should point to repo with built async-profiler)
+
+Record a simple JFR trace via
+
+./profile.py jfr -f file.jfr -- -cp test AllocatingTarget
+
+and a simple async-profiler trace via
+
+./profile.py async -s -f file.jfr -- -cp test AllocatingTarget
+
+produce a flamegraph via
+
+./profile.py async -f file.html -t flamegraph -- -cp test AllocatingTarget
+
+Requires the click package
+"""
+
+
+def execute_java(args: List[str]):
+    cmd = f"java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints  {' '.join(shlex.quote(arg) for arg in args)}"
+    print(cmd)
+    os.system(cmd)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(help="Run Java with JFR")
+@click.option("-i", "--interval", default=None)
+@click.option("-s", "--settings", type=str, default="default.jfc")
+@click.option("-f", "--file", type=Path, required=True)
+@click.argument("java_args", nargs=-1)
+def jfr(interval: Optional[str], settings: Path, file: Path, java_args: List[str]):
+    parts = [f"filename={file}", f"settings={settings}"]
+    if interval is not None:
+        parts.extend(["jdk.ExecutionSample#period=" + interval, "jdk.NativeMethodSample#period=" + interval])
+    execute_java(["-XX:StartFlightRecording=" + ','.join(parts)] + list(java_args))
+
+
+@cli.command(help="Run Java with async-profiler")
+@click.option("-i", "--interval", default="10ms")
+@click.option("-f", "--file", type=Path, required=True)
+@click.option("-e", "--event", default="cpu", help="cpu, alloc, lock, cache-misses, ...")
+@click.option("-t", "--type", default=None, type=click.Choice([None, "jfr", "collapsed", "flamegraph", "tree"]))
+@click.option("-s", "--jfrsync", default=False, show_default=True, is_flag=True,
+              help=" start Java Flight Recording with the given configuration synchronously with the profiler")
+@click.argument("java_args", nargs=-1)
+def ap(interval: str, file: Path, event: str, type: str, jfrsync: bool, java_args: List[str]):
+    args = f"-agentpath:{os.getenv('ASYNC_PROFILER')}/build/libasyncProfiler.so=start,interval={interval}," \
+           f"event={event},file={file}"
+    if type is not None:
+        if file.name.endswith(".jfr"):
+            type = "jfr"
+        elif file.name.endswith("html"):
+            type = "flamegraph"
+    if type is not None:
+        args += "," + type
+    if jfrsync:
+        args += ",jfrsync"
+    execute_java([args] + list(java_args))
+
+
+@cli.command(help="Convert JFR to flamegraph")
+@click.argument("input", type=Path)
+@click.argument("output", type=Path)
+def jfr2flame(input: Path, output: Path):
+    cmd = f"java -cp {os.getenv('ASYNC_PROFILER')}/build/converter.jar jfr2flame {input} {output}"
+    print(cmd)
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    cli()
